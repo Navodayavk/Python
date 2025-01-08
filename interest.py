0330f0b841145ba31cf807e8a2372978b569fde5
Navodaya <navodaya.kallur@s.amity.edu>
2025-01-08 23:53:53 +0530

    interest.py

---
+++ interest.py
@@ -0,0 +1,4 @@
+#Python Program to calculate simple interest
+p = float(input("Enter principal amount: "))
+r = float(input("Enter annual interest rate (in percentage): "))
+t = float(input("Enter time in years: "))
