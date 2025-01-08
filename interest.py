25785c46c3c8b44006867d66bf2906b5b2166abf
Navodaya <navodaya.kallur@s.amity.edu>
2025-01-08 23:54:48 +0530

    interest.py

--- interest.py
+++ interest.py
@@ -2,3 +2,6 @@
 p = float(input("Enter principal amount: "))
 r = float(input("Enter annual interest rate (in percentage): "))
 t = float(input("Enter time in years: "))
+simple_interest = (p * r * t) / 100
+
+print("Simple interest:", simple_interest)
